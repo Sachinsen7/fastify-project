822cad56082e416c4ba99de05dcd87d0df06093c
Sachin Sen <sachinsen1920@gmail.com>
2025-09-14 14:15:44 +0530

    add usermodel

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpiry: Date,
+});
+
+module.exports = mongoose.model("User", userSchema);
